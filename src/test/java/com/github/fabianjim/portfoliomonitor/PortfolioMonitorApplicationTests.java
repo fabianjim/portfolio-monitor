9d90f9aa312ccc25d9760190776bf55e606d04d9
Fabian Jimenez <fabian@Mac.lan>
2025-07-23 22:55:54 -0400

    Initial commit

---
+++ src/test/java/com/github/fabianjim/portfoliomonitor/PortfolioMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabianjim.portfoliomonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
