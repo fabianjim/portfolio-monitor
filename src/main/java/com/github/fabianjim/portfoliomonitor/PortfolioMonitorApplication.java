ec67a34363264d1fe35a92abf29215f13a76faa3
fabianjim <fabian.jim26@gmail.com>
2025-07-23 23:39:39 -0400

    springboot init

--- src/main/java/com/github/fabianjim/portfoliomonitor/PortfolioMonitorApplication.java
+++ src/main/java/com/github/fabianjim/portfoliomonitor/PortfolioMonitorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortfolioMonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioMonitorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioMonitorApplication.class, args);
+    }
 
 }
